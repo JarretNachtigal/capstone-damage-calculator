eb46391d8d800acaa29819804d6e59d56f1f6764
Jarret Nachtigal <jarretn1224@gmail.com>
2021-11-19 16:25:45 -0500

    crud tested, works, http gem, cors, bcrypt in gemfile

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -1,9 +1,9 @@
 class CalculationsController < ApplicationController
   def create
     calculation = Calculation.new(
-      champion_id_two = params[:champion_id_one],
-      champion_id_one = params[:champion_id_two],
-      ability_id = params[:ability_id],
+      champion_id_one: params[:champion_id_one],
+      champion_id_two: params[:champion_id_two],
+      ability_id:  params[:ability_id],
     )
     if calculation.save
       render json: calculation
@@ -18,9 +18,16 @@ class CalculationsController < ApplicationController
   end
 
   def update
+    calculation = Calculation.find(params[:id])
     calculation.champion_id_one = params[:champion_id_one] || calculation.champion_id_one
     calculation.champion_id_two = params[:champion_id_two] || calculation.champion_id_two
     calculation.ability_id = params[:ability_id] || calculation.ability_id
+
+    if calculation.save
+      render json: calculation
+    else
+      render json: { message: "no" }
+    end
   end
 
   def show
