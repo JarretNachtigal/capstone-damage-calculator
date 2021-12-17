1744876090c4004daaaaba083f7b6fbc26f12245
Jarret Nachtigal <jarretn1224@gmail.com>
2021-12-17 15:09:32 -0500

    calculation serializer, adds items

--- app/serializers/calculation_serializer.rb
+++ app/serializers/calculation_serializer.rb
@@ -1,3 +1,3 @@
 class CalculationSerializer < ActiveModel::Serializer
-  attributes :id, :champion_id_one, :champion_id_two, :ability_id, :output, :champ_one_level, :champ_two_level, :ability_level, :defending_champion_current_hp
+  attributes :id, :champion_id_one, :champion_id_two, :ability_id, :output, :champ_one_level, :champ_two_level, :ability_level, :defending_champion_current_hp, :attacking_item_id_one, :attacking_item_id_two, :attacking_item_id_three, :attacking_item_id_four, :attacking_item_id_five, :attacking_item_id_six, :defending_item_id_one, :defending_item_id_two, :defending_item_id_three, :defending_item_id_four, :defending_item_id_five, :defending_item_id_six
 end
